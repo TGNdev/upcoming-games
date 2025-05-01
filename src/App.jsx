7a7c7bed2877c2807bd834c9301890ddb1b467c7
Thomas Grossmann <thomas.grossmann@eduvaud.ch>
2025-05-01 11:30:47 +0200

    Update App title

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { HashRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 
 const App = () => {
+  document.title = process.env.REACT_APP_TITLE;
+
   return <HashRouter>
     <div className="min-h-screen flex flex-col">
       <div className="sticky top-0 bg-white z-50">
         <nav className="flex justify-between items-center px-6 py-4 shadow">
           <Link to="/">
-            <h1 className="text-xl font-bold">Game Track 2025</h1>
+            <h1 className="text-xl font-bold">{process.env.REACT_APP_TITLE}</h1>
           </Link>
         </nav>
       </div>
